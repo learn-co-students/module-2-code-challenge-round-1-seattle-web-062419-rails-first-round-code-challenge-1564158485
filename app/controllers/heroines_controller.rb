d25ed6a3051d8e21a6140f4128e7f986c69d80f9
Larysa Cohee <larysa.cohee@flatironschool.com>
2019-07-26 11:00:11 -0700

    got everying working but render new I think

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,34 @@
 class HeroinesController < ApplicationController
+  # def index
+  #   @heroines = Heroine.all
+  # end
+
   def index
-    @heroines = Heroine.all
+    @heroines = Heroine.search(params[:search])
   end
+
+  def new
+    @powers = Power.all
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save!
+      redirect_to @heroine
+    else
+      render :new 
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  private 
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
